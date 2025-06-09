9dffec3f001db7dc337d6655b739b3df32a791f2
Jose Melo <jmelo@live.cl>
2025-06-09 20:57:31 +0200

    ad

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <cstring>
-#include "MemoryManager.h"
 #include "visitorHeaders.h"
+#include "scriptingParser.h"
+#include "scriptingProduct.h"
 
+using Date = int;
 int main()
 {
-    std::string script = "test";
-    
+    string s = "X = 3";
+    Date today = 1;
+    map<Date, string> mapping = {{today, s}};
+    Product prod;
+    prod.parseEvents(mapping.begin(), mapping.end());
+    prod.indexVariables();
+    Debugger d;
+    prod.visit(d);
+    cout << d.getString() << endl;
+    std::unique_ptr<Scenario<double>> scenario = prod.buildScenario<T>();
+    std::unique_ptr<Evaluator<double>> evaluator = prod.buildEvaluator<T>();
+    prod.evaluate(*scenario, *evaluator);
+    std::vector<double> values = evaluator->varVals();
+    std::vector<string> var_names = prod.varNames();
+    for (size_t i = 0; i < values.size(); i++)
+    {
+        cout << var_names[i] << ": " << values[i] << endl;
+    };
 }
